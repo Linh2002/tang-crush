8875d227e7abe048317e989b10d6dfee76476051
Linh2002 <86980585+Linh2002@users.noreply.github.com>
2021-07-06 01:19:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,18 +2,18 @@ const CONFIG = {
     introTitle: 'Khải Khùng!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Lạc vào tim chị lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hehe',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'thích chị cực <33',
     btnNo: 'Không, chị gái à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại thích cơ chứ <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại thích chị cơ chứ <3',
     btnReply: 'Gửi cho chị nhé <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Chị biết mà 🥰 Ngại ghê :v 😘😘',
-    messDesc: 'Ngủ ddeeeeee .',
+    messDesc: ' ngủ đi mẹ .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
